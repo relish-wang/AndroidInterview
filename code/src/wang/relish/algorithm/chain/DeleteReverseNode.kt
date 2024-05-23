95aa078aaa95fe3e7117be7b052a4938fbfb2d7f
relish-wang <relish.wang@gmail.com>
2024-05-23 15:52:10 +0800

    整理

--- code/src/wang/relish/algorithm/chain/DeleteReverseNode.kt
+++ code/src/wang/relish/algorithm/chain/DeleteReverseNode.kt
@@ -2,25 +2,6 @@ package wang.relish.algorithm.chain
 
 import wang.relish.algorithm.common.ListNode
 
-
-/**
- * 删除单向链表中的第K个节点
- *
- * 时间复杂度: O(n)
- * 空间复杂度: O(n)
- *
- * @param head 单向链表
- * @param k 删除倒数第k个节点
- */
-private fun deleteReverseNode1(head: ListNode?, k: Int) {
-    var t: ListNode? = head
-    val list = arrayListOf<ListNode>()
-    while (t != null) {
-        list.add(t)
-        t = t.next
-    }
-    list[list.size - k - 1].next = list[list.size - k + 1]
-}
 /**
  * 删除单向链表中的第K个节点
  *
@@ -45,6 +26,26 @@ private fun deleteReverseNode(head: ListNode?, k: Int) {
         fast = fast.next
     }
     pre?.next = slow?.next
+}
+
+
+/**
+ * 删除单向链表中的第K个节点
+ *
+ * 时间复杂度: O(n)
+ * 空间复杂度: O(n)
+ *
+ * @param head 单向链表
+ * @param k 删除倒数第k个节点
+ */
+private fun deleteReverseNode1(head: ListNode?, k: Int) {
+    var t: ListNode? = head
+    val list = arrayListOf<ListNode>()
+    while (t != null) {
+        list.add(t)
+        t = t.next
+    }
+    list[list.size - k - 1].next = list[list.size - k + 1]
 }
 
 fun main() {
